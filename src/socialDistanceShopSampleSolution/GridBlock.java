a9eb5899845e5ffabac446b2aa7818218e9d246c
Tino <tinomuzambi@gmail.com>
2020-06-20 22:13:48 +0200

    Adds semaphores to get.

--- src/socialDistanceShopSampleSolution/GridBlock.java
+++ src/socialDistanceShopSampleSolution/GridBlock.java
@@ -2,14 +2,17 @@ package socialDistanceShopSampleSolution;
 
 // GridBlock class to represent a block in the shop.
 
+import java.util.concurrent.Semaphore;
+
 public class GridBlock {
     private boolean isOccupied;
     private final boolean isExit;
     private final boolean isCheckoutCounter;
     private int [] coords; // the coordinate of the block.
-    private int ID;
+    private final int ID;
 
     public static int classCounter=0;
+    private Semaphore mutex;
 
     GridBlock(boolean exitBlock, boolean checkoutBlock) throws InterruptedException {
         isExit=exitBlock;
@@ -17,32 +20,36 @@ public class GridBlock {
         isOccupied= false;
         ID=classCounter;
         classCounter++;
+        mutex = new Semaphore(1);
     }
 
     GridBlock(int x, int y, boolean exitBlock, boolean refreshBlock) throws InterruptedException {
         this(exitBlock,refreshBlock);
         coords = new int [] {x,y};
+        mutex = new Semaphore(1);
     }
 
     //getter
-    public  int getX() {
+    public int getX() {
         return coords[0];
     }
 
     //getter
-    public  int getY() {
+    public int getY() {
         return coords[1];
     }
 
     //for customer to move to a block
     public boolean get() throws InterruptedException {
-        isOccupied=true;
+        mutex.acquire();
+        isOccupied = true;
+        mutex.release();
         return true;
     }
 
     //for customer to leave a block
-    public  void release() {
-        isOccupied =false;
+    public void release() {
+        isOccupied = false;
     }
 
     //getter
@@ -51,12 +58,12 @@ public class GridBlock {
     }
 
     //getter
-    public  boolean isExit() {
+    public boolean isExit() {
         return isExit;
     }
 
     //getter
-    public  boolean isCheckoutCounter() {
+    public boolean isCheckoutCounter() {
         return isCheckoutCounter;
     }
 
